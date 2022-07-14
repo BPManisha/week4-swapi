d4521afbd12422f0b1da81ebf49dec4feb79418e
BPManisha <107884131+BPManisha@users.noreply.github.com>
2022-07-13 22:41:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const button = document.querySelector('button')
+const nameContainer =document.createElement('div'); 
+const buttonClick =() => {
+    axios.get("https://swapi.dev/api/planets/?search=Alderaan")
+    .then((res)=>{ 
+          
+        nameContainer.textContent='';
+        for (let i=0 ; i < res.data.results[0].residents.length; i++)
+        {  
+            axios.get(res.data.results[0].residents[i]).then((res)=>
+            {
+                const resident = document.createElement(`h2`)
+                resident.textContent='';
+                resident.textContent = res.data.name;
+                //resident.textContent ='';
+                resident.textContent=res.data.name+' and height is:  '+res.data.height
+                nameContainer.appendChild(resident)
+            })
+            .catch( error => {console.log(error)})
+        } })
+        .catch( error => {console.log(error)})
+        document.body.appendChild(nameContainer);
+    
+}
+button.addEventListener('click',buttonClick)
